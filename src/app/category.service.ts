2bc4b015ba5d88344750761283d8e78c5b85eef0
Simona Lorenc <lorenc.simona@gmail.com>
2023-09-29 09:52:55 +0200

    add remove filters

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
@@ -6,14 +6,16 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class CategoryService {
   selectedCategoriesSubject = new BehaviorSubject<string[]>([])
-  //selectedCategories = this.selectedCategoriesSubject.asObservable()
 
   addCategory(category: string) {
     const currentCategories = this.selectedCategoriesSubject.value
-    console.log("TAG_categoryService: " + currentCategories)
     if(!currentCategories.includes(category)) {
       currentCategories.push(category)
       this.selectedCategoriesSubject.next(currentCategories)
     }
+  }
+
+  setCategories(categories: string[]) {
+    this.selectedCategoriesSubject.next(categories)
   }
 }
--- src/app/job/job.component.ts
+++ src/app/job/job.component.ts
@@ -29,14 +29,32 @@ export class JobComponent implements OnInit {
       .asObservable()
       .subscribe((categories) => {
         this.selectedCategories = categories
-        this.filterResuts();
+        this.filterResults();
       });
   }
 
-  filterResuts(): void {
+  filterResults(): void {
     if (this.selectedCategories.length == 0) return
     this.filteredJobsList = this.jobOffersList.filter(jobOffer => {
       return this.selectedCategories.every(selected => jobOffer.categories.includes(selected))
     })
+  }
+
+  removeCategory(event: any) {
+    const clickedCategory = event.target.parentNode.textContent
+    const categoryToDelete = clickedCategory.slice(0, -1)
+    const indexToDelete = this.selectedCategories.indexOf(categoryToDelete)
+    if(indexToDelete !== -1) {
+      this.selectedCategories.splice(indexToDelete, 1)
+    }
+    this.filterResults()
+    if(this.selectedCategories.length === 0 )
+      this.filteredJobsList = this.jobOffersList
+  }
+
+  removeAllCategories() {
+    this.selectedCategories = []
+    this.categoryService.setCategories(this.selectedCategories)
+    this.filteredJobsList = this.jobOffersList
   }
 }
